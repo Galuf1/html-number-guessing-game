f1014c9d9e35a802d62d2f8a1679ecb8da9a66f8
Galuf1 <daniel.pizarro.farias@gmail.com>
2022-06-25 17:26:19 +0200

    no css

--- app.js
+++ app.js
@@ -1,2 +1,32 @@
 console.log("HELLO PAPA PLATOON!")
 // Your function(s) should go here that will interact with the webpage or DOM
+
+const rnd = Math.floor(Math.random() * 100)
+
+const printNumber = function(str){
+    const list = document.getElementById("guess_list")
+    const value = document.createElement("p")
+    value.innerHTML = str
+
+    list.appendChild(value)
+}
+const guess = function(e) {
+    
+    const guessed = parseInt(document.getElementById("guess").value)
+
+    if (guessed === rnd) {
+        printNumber(`${guessed} was the number`)
+    } else if ( guessed <= rnd) {
+        alert("too low")
+        printNumber(guessed)
+    } else if (guessed >= rnd) {
+        printNumber(guessed)
+        alert ("too high")
+    }
+
+
+
+
+    // console.log(rnd)
+    // console.log(typeof guessed)
+}
